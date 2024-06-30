5419ddaf56df9a637e98a1491e32303bcff4c4df
Itsrajsk <rajkashid146303@gmail.com>
2024-06-30 22:59:48 +0530

    Added Pattern 22

---
+++ Patterns/Pattern-22.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void pattern22(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - i;
+            cout << (n - min(min(top, down), min(left, right)));
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "\nEnter Size Of Pattern : " << endl;
+    cin >> n;
+    cout << endl;
+    pattern22(n);
+    cout << endl;
+    return 0;
+}
