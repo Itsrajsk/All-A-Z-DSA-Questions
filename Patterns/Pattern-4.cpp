32e360a9a161eb0213342858f93a9630f882bf38
Itsrajsk <rajkashid146303@gmail.com>
2024-06-30 22:55:48 +0530

    Added Pattern 4

---
+++ Patterns/Pattern-4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void pattern4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "\nEnter Size Of Pattern : " << endl;
+    cin >> n;
+    cout << endl;
+    pattern4(n);
+    cout << endl;
+    return 0;
+}
