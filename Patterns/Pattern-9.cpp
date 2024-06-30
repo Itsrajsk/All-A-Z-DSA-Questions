8a6639fc03f203e695735d8746b1103e6766b52f
Itsrajsk <rajkashid146303@gmail.com>
2024-06-30 22:56:51 +0530

    Added Pattern 9

---
+++ Patterns/Pattern-9.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+void pattern7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - (i + 1); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < (2 * i) + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - (i + 1); j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < (2 * n) - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "\nEnter Size Of Pattern : " << endl;
+    cin >> n;
+    cout << endl;
+    pattern7(n);
+    pattern8(n);
+    cout << endl;
+    return 0;
+}
