521ed158cd68e0735dc9d0ffff8395f64f6439e6
Itsrajsk <rajkashid146303@gmail.com>
2024-06-30 22:57:54 +0530

    Added Pattern 13

---
+++ Patterns/Pattern-13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void pattern13(int n)
+{
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << count++ << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "\nEnter Size Of Pattern : " << endl;
+    cin >> n;
+    cout << endl;
+    pattern13(n);
+    cout << endl;
+    return 0;
+}
