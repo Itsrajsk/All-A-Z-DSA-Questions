2e5bf3ffd1de845c1745468ddae0bca1a7c430f6
Itsrajsk <rajkashid146303@gmail.com>
2024-06-30 22:57:07 +0530

    Added Pattern 10

---
+++ Patterns/Pattern-10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void pattern10(int n)
+{
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+            stars = 2 * n - i;
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "\nEnter Size Of Pattern : " << endl;
+    cin >> n;
+    cout << endl;
+    pattern10(n);
+    cout << endl;
+    return 0;
+}
