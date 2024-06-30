52a7c6842cadcf67fa79229a76e46fa6867b3e0c
Itsrajsk <rajkashid146303@gmail.com>
2024-06-30 22:55:17 +0530

    Added Pattern 2

---
+++ Patterns/Pattern-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "\nEnter Size Of Pattern : " << endl;
+    cin >> n;
+    cout << endl;
+    pattern2(n);
+    cout << endl;
+    return 0;
+}
