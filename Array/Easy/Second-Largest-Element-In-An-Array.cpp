8cdb17bba5dadc1e0cf792925d39069aaf749c63
Itsrajsk <rajkashid146303@gmail.com>
2024-07-08 20:51:26 +0530

    Added Second Largest Element In An Array

---
+++ Array/Easy/Second-Largest-Element-In-An-Array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printSecondLargest(int arr[], int n)
+{
+    int largest = arr[0];
+    int slargest = -1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            slargest = largest;
+            largest = arr[i];
+        }
+    }
+    cout << "\nSecond Largest : " << slargest << endl;
+}
+int main()
+{
+    cout << endl;
+    int arr[] = {3, 2, 1, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Array Elements : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    printSecondLargest(arr, n);
+    cout << endl;
+    return 0;
+}
