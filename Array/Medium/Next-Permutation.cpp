e16491622242389150f752a82943d87e29d8e169
Itsrajsk <rajkashid146303@gmail.com>
2024-07-18 21:49:32 +0530

    Added Next Permutation

---
+++ Array/Medium/Next-Permutation.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void nextPermutation(vector<int> &nums)
+{
+    int ind = -1;
+    int n = nums.size();
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (nums[i] < nums[i + 1])
+        {
+            ind = i;
+            break;
+        }
+    }
+    if (ind == -1)
+    {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+    for (int i = n - 1; i > ind; i--)
+    {
+        if (nums[i] > nums[ind])
+        {
+            swap(nums[i], nums[ind]);
+            break;
+        }
+    }
+    reverse(nums.begin() + ind + 1, nums.end());
+}
+
+int main()
+{
+    // Example test case
+    vector<int> nums = {1, 2, 3};
+
+    cout << "Original array: ";
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    nextPermutation(nums);
+
+    cout << "Next permutation: ";
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
